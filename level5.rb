456eca7d377df18c83aa95b5c3383b43f08eda28
stepozer <stepozer@gmail.com>
2014-05-14 22:51:35 +0400

    level 5

--- level5.rb
+++ level5.rb
@@ -2,16 +2,14 @@ class Player
 	def play_turn(warrior)
 		@state ||= '1'
 		space = warrior.feel
-		if (@state == '1' and space.captive?)
+		if (@state == '1' and space.empty? and warrior.health < 20)
 			@state = '2'
-		elsif (@state == '2' and space.empty? and warrior.health < 20)
-			@state = '3'
-		elsif (@state == '3' and space.empty?)
+		elsif (@state == '2' and space.empty?)
 			if (warrior.health < 20)
 				warrior.rest!
 				return nil
 			else
-				@state = '5'
+				@state = '3'
 			end
 		end
 		if (space.empty?)
