82f145ba0fbaba692e19b27fb582915005077430
stepozer <stepozer@gmail.com>
2014-05-14 22:46:41 +0400

    livel 5

--- level5.rb
+++ level5.rb
@@ -2,46 +2,24 @@ class Player
 	def play_turn(warrior)
 		@state ||= '1'
 		space = warrior.feel
-		if (@state == '1')
-			if (space.empty?)
-				warrior.walk!
-			elsif (space.captive?)
-				warrior.rescue!
-				@state = '2'
-			end
-		elsif (@state == '2')
-			if (space.empty?)
-				warrior.walk!
-				if (warrior.health < 20)
-					@state = '3'
-				end
-			elsif (space.enemy?)
-				warrior.attack!
-			end
-		elsif (@state == '3')
-			if (space.empty?)
-				if (warrior.health < 20)
-					@state = '4'
-				else
-					warrior.walk!
-				end
-			elsif (space.enemy?)
-				warrior.attack!
-			end
-		elsif (@state == '4')
+		if (@state == '1' and space.captive?)
+			@state = '2'
+		elsif (@state == '2' and space.empty? and warrior.health < 20)
+			@state = '3'
+		elsif (@state == '3' and space.empty?)
 			if (warrior.health < 20)
 				warrior.rest!
+				return nil
 			else
 				@state = '5'
 			end
-		elsif (@state == '5')
-			if (space.empty?)
-				warrior.walk!
-			elsif (space.enemy?)
-				warrior.attack!
-			elsif (space.captive?)
-				warrior.rescue!
-			end
+		end
+		if (space.empty?)
+			warrior.walk!
+		elsif (space.enemy?)
+			warrior.attack!
+		elsif (space.captive?)
+			warrior.rescue!
 		end
 	end
 end  
