9c2cdc35dc2d4c65d1804019e6ca783fab24f452
dstepanenko <studos.work@gmail.com>
2014-05-15 18:39:06 +0400

    level 6

---
+++ level6.rb
@@ -0,0 +1,33 @@
+class Player
+	def play_turn(warrior)
+		@state     ||= :backward
+		@direction ||= :backward
+		
+		space = warrior.feel @direction
+
+		if (@state == :forward and warrior.health < 10) 
+			warrior.walk! :backward
+			@state = :rest
+			return nil;
+		end
+		if (@state == :rest) 
+			if (warrior.health < 20)
+				warrior.rest!
+			else
+				@state = :kill_all
+			end
+			return nil;
+		end
+
+			
+		if (space.empty?)
+			warrior.walk! @direction
+		elsif (space.enemy?)
+			warrior.attack!
+		elsif (space.captive?)
+			warrior.rescue! @direction
+			@state     = :forward
+			@direction = :forward
+		end
+	end
+end  
